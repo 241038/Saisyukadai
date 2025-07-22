f157ac78e3dcc56ae92aa6ded7b84b3acfaa35bc
unknown <241038>
2025-07-22 18:59:48 +0900

    Initial commit

---
+++ src/Character.java
@@ -0,0 +1,31 @@
+public abstract class Character implements Creature {
+    private String name;
+    private int hp;
+
+    Character(String name, int hp) {
+        this.name = name;
+        this.hp = hp;
+    }
+
+    public boolean isAlive() {
+        if (hp > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void showStatus() {
+        System.out.println(name + ":hp" + hp);
+    }
+
+    public String getName() {
+        return this.name;
+    }
+    public int getHp() {
+        return this.hp;
+    }
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+}
